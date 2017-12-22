d91f871d373be30ac83ac98b8f2cdb73e26d1d78
Andrew McLaughlin <andrew@mclau.ghlin.com>
2017-12-22 14:58:20 -0800

    Added logic for outputting mbox files, still buggy

--- mbox_redactor.py
+++ mbox_redactor.py
@@ -10,33 +10,51 @@ import crayons
 
 __version__ = '0.0.1'
 
-mbox_path  = './mbox_files/'
-mbox_files = os.listdir(mbox_path)
-
 def print_headers(headers):
     for key, value in headers:
         print(f'Key: {key} \nValue: {value}')
 
+def set_headers(headers, mbox_new):
+    for key, value in headers:
+        mbox_new[key] = value
+
+mbox_path     = './mbox_files/'
+mbox_path_new = './mbox_files_new/'
+mbox_files    = os.listdir(mbox_path)
+
+if not os.path.exists(mbox_path_new):
+    os.makedirs(mbox_path_new)
 
 for filename in mbox_files:
 
     if (filename[-4:] == 'mbox'):
 
-        mbox_file = f'{mbox_path}/{filename}'
-        mbox = mailbox.mbox(mbox_file)
+        mbox_file       = f'{mbox_path}/{filename}'
+        mbox_file_new   = f'{mbox_path_new}/{filename[:-5]}.new.box'
+
+        mbox            = mailbox.mbox(mbox_file)
+        mbox_new        = mailbox.mbox(mbox_file_new)
+        mbox_new.lock()
 
         for key, value in mbox.iteritems():
 
-            print(crayons.red(str(key)))
-            message = mbox[key]
-            unix_from = message.get_unixfrom()
-            headers = message.items()
-            values  = message.values()
+            #get message input
+            message     = mbox[key]
+            unixfrom    = message.get_unixfrom()
+            headers     = message.items()
+            values      = message.values()
 
+            #terminal output
             print('\n\n')
+            print(f'Message #: {crayons.red(str(key))}')
             print(crayons.green(f'filename: {filename}'))
-            print(f'unixfrom: {unix_from}')
+            print(f'unixfrom: {unixfrom}')
             print_headers(headers)
+
+            #set output
+            message_new = mbox_new[key]
+            message_new.set_unixfrom(unixfrom)
+            set_headers(headers, mbox_new)
 
             if message.is_multipart():
 
@@ -46,9 +64,15 @@ for filename in mbox_files:
 
                 for payload in multipart_payload:
 
+                    payload_decoded = payload.get_payload(decode=True)
+
                     print(crayons.yellow(40*'-'))
                     print_headers(payload.items())
-                    print(crayons.cyan(payload.get_payload(decode=True)))
+                    print(crayons.cyan(payload_decoded))
+
+                    #set output
+                    set_headers(payload.items(), mbox_new)
+                    message_new.set_payload(payload_decoded)
                     
             else:
 
@@ -57,5 +81,12 @@ for filename in mbox_files:
                 print(payload)
                 print(40*'#')
 
+                #set output
+                message_new.set_payload(payload)
+
+        #write file and finish up
+        mbox_new.add(message_new)
+        mbox_new.flush
+        mbox_new.unlock
 
 
