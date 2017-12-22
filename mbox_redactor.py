b9219ecc4a3a407fd0c7bfb6d1b57e4073df6824
Andrew McLaughlin <andrew@mclau.ghlin.com>
2017-12-21 16:03:40 -0800

    Wrote basic code for reading and printing mbox headers and payload.

--- mbox_redactor.py
+++ mbox_redactor.py
@@ -6,11 +6,17 @@ Script used to redact words from email in mbox format
 
 import mailbox
 import os
+import crayons
 
 __version__ = '0.0.1'
 
 mbox_path  = './mbox_files/'
 mbox_files = os.listdir(mbox_path)
+
+def print_headers(headers):
+    for key, value in headers:
+        print(f'Key: {key} \nValue: {value}')
+
 
 for filename in mbox_files:
 
@@ -19,7 +25,37 @@ for filename in mbox_files:
         mbox_file = f'{mbox_path}/{filename}'
         mbox = mailbox.mbox(mbox_file)
 
-        for msg in mbox:
-            print(msg['subject'])
+        for key, value in mbox.iteritems():
+
+            print(crayons.red(str(key)))
+            message = mbox[key]
+            unix_from = message.get_unixfrom()
+            headers = message.items()
+            values  = message.values()
+
+            print('\n\n')
+            print(crayons.green(f'filename: {filename}'))
+            print(f'unixfrom: {unix_from}')
+            print_headers(headers)
+
+            if message.is_multipart():
+
+                multipart_payload = message.get_payload()
+
+                print(crayons.yellow(f'multipart payload: {multipart_payload}'))
+
+                for payload in multipart_payload:
+
+                    print(crayons.yellow(40*'-'))
+                    print_headers(payload.items())
+                    print(crayons.cyan(payload.get_payload(decode=True)))
+                    
+            else:
+
+                payload = message.get_payload(decode=True)
+
+                print(payload)
+                print(40*'#')
+
 
 
