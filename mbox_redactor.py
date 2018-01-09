06c39dc6d3bed87f7fa3753ed6e4f7e8fba739f0
Andrew McLaughlin <andrew@mclau.ghlin.com>
2018-01-09 00:26:07 -0800

    Employed message.walk() to more thoroughly traverse message object tree

--- mbox_redactor.py
+++ mbox_redactor.py
@@ -20,106 +20,86 @@ if os.path.exists(mbox_path_new):
 
 os.makedirs(mbox_path_new)
 
-def print_headers(headers):
-    for key, value in headers:
-        print(f'Key: {key} \nValue: {value}')
+def set_headers(headers, msg_new):
+#    print(headers)
+#    input('press key')
+    for key, value in headers: 
+        msg_new[key] = value
+#        print(f'{key}: {value}')
+#        input('press key')
+    return msg_new
 
-def set_headers(headers, message_new):
-    for key, value in headers:
-        message_new[key] = value
-    return message_new
+def process_payload(headers, payload, msg_new):
+    msg_new = set_headers(headers, msg_new)
+    msg_new.set_payload(payload)
+    return msg_new
 
-def process_payload(message_new, headers, payload):
-    message_new = set_headers(headers, message_new)
-    message_new.set_payload(payload)
-    return message_new
+def process_multipart(msg, msg_new):
+    for part in msg.get_payload():
+        msg_new = process_message(part, msg_new)
+    return msg_new
 
-def process_multipart(message, message_new):
+def process_message(msg, msg_new):
+    content_type = msg.get_content_type()
+    charset = msg.get_content_charset()
+#    msg_new = set_headers(msg.items(), msg_new)
 
-    multipart_payload = message.get_payload()
+#    print(f'is_multipart: {msg.is_multipart()}')
+#    print(f'content_type: {content_type}')
+#    print(f'charset: {charset}')
+#    input('click')
+#    print(f'payload: {msg.get_payload()}')
+#    input('click')
 
-    for part in multipart_payload:
-
-        headers = part.items()
-        message_new = set_headers(headers, message_new) 
-        message_new = process_message(part, message_new)
-
-    return message_new
-
-def process_message(message, message_new):
-
-    if message.is_multipart():
-
-        new_message = process_multipart(message, message_new)
-
+    if msg.is_multipart():
+        if not isinstance(msg, str):
+            msg_new = set_headers(msg.items(), msg_new)        
+        msg_new = process_multipart(msg, msg_new)
     else:
-
-        content_type = message.get_content_type()
-        charset = message.get_content_charset()
-        message_new = set_headers(message.items(), message_new)
-
-
-        if not charset:
-            payload = message.get_payload()
+        msg_new = set_headers(msg.items(), msg_new)
+        if not charset or content_type == 'text/plain':
+            #usually attachments
+            payload = msg.get_payload()
         else:
-            payload = str(message.get_payload(decode=True), str(charset), "ignore")
-
+            payload = str(msg.get_payload(decode=True))
             if payload.startswith("b'") and payload.endswith("'"):
                 payload =  payload[2:-1]
-
-            payload = str(payload).encode('utf8', 'replace')
-
-#        if not charset:
-#            #usually attachments
-#            payload = str(message.get_payload())
-##            print(content_type)
-#        else:
-#            payload = str(payload)
-#            if payload.startswith("b'") and payload.endswith("'"):
-#                payload =  payload[2:-1]
-
-        message_new.set_payload(payload.encode('utf-8'))
-
-    return message_new
-
+        msg_new.set_payload(payload)
+    return msg_new
 
 for filename in mbox_files:
-
     if (filename[-4:] == 'mbox'):
 
         mbox_file       = f'{mbox_path}/{filename}'
         mbox_file_new   = f'{mbox_path_new}/{filename[:-5]}.new.mbox'
-
         mbox            = mailbox.mbox(mbox_file)
         mbox_new        = mailbox.mbox(mbox_file_new)
         mbox_new.lock()
 
         for key, value in mbox.iteritems():
-
             try:
-
-                #get message input
-                message     = mbox[key]
-                unixfrom    = message.get_unixfrom()
-                headers     = message.items()
+                #get msg input
+                msg      = mbox[key]
+                unixfrom = msg.get_unixfrom()
+                #headers  = msg.items()
 
                 #output
-                message_new = mailbox.mboxMessage()
-                message_new.set_unixfrom(unixfrom)
-                message_new = set_headers(headers, message_new)
-                message_new = process_message(message, message_new)
+                msg_new  = mailbox.mboxMessage()
+                msg_new.set_unixfrom(unixfrom)
+
+                if msg.is_multipart():
+                    for part in msg.walk():
+                        msg_new = process_message(part, msg_new)
+                else:
+                    msg_new = process_message(msg, msg_new)
 
                 #write file and finish up
-                mbox_new.add(message_new)
+                mbox_new.add(msg_new)
 
-            except (KeyError, UnicodeEncodeError) as e:
+            except (AttributeError, KeyError, UnicodeEncodeError) as e:
 
                 print(crayons.magenta(f"Error for '{mbox_file_new}' {e}"))
                 continue
 
         mbox_new.flush
         mbox_new.unlock()
-
-
- 
-
