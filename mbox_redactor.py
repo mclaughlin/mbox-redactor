a08b56da2f159fe75eecadbeeebc4157d79c515a
Andrew McLaughlin <andrew@mclau.ghlin.com>
2018-01-11 13:59:02 -0800

    Refactored single and multipart functions

--- mbox_redactor.py
+++ mbox_redactor.py
@@ -35,94 +35,51 @@ def process_payload(headers, payload, msg_new):
     msg_new.set_payload(payload)
     return msg_new
 
-def process_multipart(msg, msg_new):
-    for part in msg.get_payload():
-        msg_new = process_message(part, msg_new)
+def multipart_message(msg, msg_new):
+    if isinstance(msg, mailbox.mboxMessage):
+        for part in msg.get_payload():
+            msg_new = process_message(part, msg_new)
+    elif isinstance(msg, email.message.Message):
+        for part in msg.get_payload():
+            print(part)
+#            if isinstance(part, email.message.Message):
+
+                #usually attachments
+#                payload = part.get_payload(decode=True) 
+
+#                if isinstance(payload, list):
+#                    for subpart in payload:
+#                        msg_new = single_message(subpart, msg_new)
+#                else:
+#                    #kathy
+#                    msg_new = single_message(part, msg_new)
+#            else:
+#                msg_new = single_message(part, msg_new)
     return msg_new
 
-def process_message(msg, msg_new):
-    msg_new = set_headers(msg.items(), msg_new)
+def single_message(msg, msg_new):
+    content_type = msg.get_content_type()
+    charset = msg.get_content_charset()
+    headers = msg.items()
+    msg_new = set_headers(headers, msg_new)
 
-#    print(f'is_multipart: {msg.is_multipart()}')
-#    print(f'content_type: {content_type}')
-#    print(f'charset: {charset}')
-#    input('click')
-#    print(f'payload: {msg.get_payload()}')
-#    input('click')
-
-    if msg.is_multipart() and isinstance(msg, mailbox.mboxMessage):
-        if not isinstance(msg, str):
-            msg_new = set_headers(msg.items(), msg_new)        
-        msg_new = process_multipart(msg, msg_new)
-#    elif msg.is_multipart():
+    if not charset or content_type == 'text/plain':
+        #usually attachments
+        payload = msg.get_payload()
     else:
-        #need to find out why this payload isn't being included
-        msg_new = set_headers(msg.items(), msg_new)
-        #for key, value in msg.items():
-        #    print(f'{key} - {value}')
-#        print(str(msg.get_payload()))
-        
-        if msg.is_multipart():
-            for part in msg.get_payload():
-                headers = part.items()
-                msg_new = set_headers(headers, msg_new)
-                #print(part.get_payload(decode=True))
-                ####
-                # process part here:
-                content_type = part.get_content_type()
-                charset = part.get_content_charset()
-                if isinstance(part, email.message.Message):
-                    #usually attachments
-                    payload = part.get_payload(decode=True) 
-                    msg_new.set_payload(payload)
-                    if isinstance(payload, list):
-                        for subpart in payload:
-#                            print(subpart.items())
-#                            print(subpart.get_payload(decode=True))
-#                            input('click')
-                            headers = subpart.items()
-                            msg_new = set_headers(headers, msg_new)
+        payload = str(msg.get_payload(decode=True))
+        if payload.startswith("b'") and payload.endswith("'"):
+            payload =  payload[2:-1]
+    msg_new.set_payload(payload)
+    return msg_new
 
-                            payload = str(part.get_payload(decode=True))
-                            if payload.startswith("b'") and payload.endswith("'"):
-                                payload =  payload[2:-1]
-                                msg_new.set_payload(payload)
  
-                    else:
-                        #this is the attachment email from Kathy
-                        #why isn't this being included???
-#                        print(part.items())
-#                        print(payload)
-                        headers = part.items()
-                        msg_new = set_headers(headers, msg_new)
-                        payload = str(payload)
+def process_message(msg, msg_new):
 
-                        if payload.startswith("b'") and payload.endswith("'"):
-                            payload = payload[2:-1]
-                            bob = msg_new
-                            msg_new.set_payload(payload)
-                            print(bob)
-                            return msg_new
-                        else:
-                            msg_new.set_payload(payload)
-                else:
-                    payload = str(part.get_payload(decode=True))
-                    if payload.startswith("b'") and payload.endswith("'"):
-                        payload =  payload[2:-1]
-                        msg_new.set_payload(payload)
-                ####
-        else:
-            content_type = msg.get_content_type()
-            charset = msg.get_content_charset()
-
-            if not charset or content_type == 'text/plain':
-                #usually attachments
-                payload = msg.get_payload()
-            else:
-                payload = str(msg.get_payload(decode=True))
-                if payload.startswith("b'") and payload.endswith("'"):
-                    payload =  payload[2:-1]
-            msg_new.set_payload(payload)
+    if msg.is_multipart():
+        msg_new = multipart_message(msg, msg_new)
+    else:
+        msg_new = single_message(msg, msg_new)
     return msg_new
 
 for filename in mbox_files:
@@ -144,12 +101,7 @@ for filename in mbox_files:
                 #output
                 msg_new  = mailbox.mboxMessage()
                 msg_new.set_unixfrom(unixfrom)
-
-                if msg.is_multipart():
-                    for part in msg.walk():
-                        msg_new = process_message(part, msg_new)
-                else:
-                    msg_new = process_message(msg, msg_new)
+                msg_new = process_message(msg, msg_new)
 
                 #write file and finish up
                 mbox_new.add(msg_new)
