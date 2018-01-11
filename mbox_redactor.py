030c518ded23ac666ffd0c90b881dad534d91609
Andrew McLaughlin <andrew@mclau.ghlin.com>
2018-01-10 21:39:34 -0800

    Added more granular filtering for files with attachments. Still needs work

--- mbox_redactor.py
+++ mbox_redactor.py
@@ -5,6 +5,7 @@ Script used to redact words from email in mbox format
 """
 
 import mailbox
+import email
 import os
 import shutil
 import crayons
@@ -40,9 +41,7 @@ def process_multipart(msg, msg_new):
     return msg_new
 
 def process_message(msg, msg_new):
-    content_type = msg.get_content_type()
-    charset = msg.get_content_charset()
-#    msg_new = set_headers(msg.items(), msg_new)
+    msg_new = set_headers(msg.items(), msg_new)
 
 #    print(f'is_multipart: {msg.is_multipart()}')
 #    print(f'content_type: {content_type}')
@@ -55,19 +54,75 @@ def process_message(msg, msg_new):
         if not isinstance(msg, str):
             msg_new = set_headers(msg.items(), msg_new)        
         msg_new = process_multipart(msg, msg_new)
-    elif msg.is_multipart():
-        #need to find out why this payload isn't being included
-        print(msg)
+#    elif msg.is_multipart():
     else:
+        #need to find out why this payload isn't being included
         msg_new = set_headers(msg.items(), msg_new)
-        if not charset or content_type == 'text/plain':
-            #usually attachments
-            payload = msg.get_payload()
+        #for key, value in msg.items():
+        #    print(f'{key} - {value}')
+#        print(str(msg.get_payload()))
+        
+        if msg.is_multipart():
+            for part in msg.get_payload():
+                headers = part.items()
+                msg_new = set_headers(headers, msg_new)
+                #print(part.get_payload(decode=True))
+                ####
+                # process part here:
+                content_type = part.get_content_type()
+                charset = part.get_content_charset()
+                if isinstance(part, email.message.Message):
+                    #usually attachments
+                    payload = part.get_payload(decode=True) 
+                    msg_new.set_payload(payload)
+                    if isinstance(payload, list):
+                        for subpart in payload:
+#                            print(subpart.items())
+#                            print(subpart.get_payload(decode=True))
+#                            input('click')
+                            headers = subpart.items()
+                            msg_new = set_headers(headers, msg_new)
+
+                            payload = str(part.get_payload(decode=True))
+                            if payload.startswith("b'") and payload.endswith("'"):
+                                payload =  payload[2:-1]
+                                msg_new.set_payload(payload)
+ 
+                    else:
+                        #this is the attachment email from Kathy
+                        #why isn't this being included???
+#                        print(part.items())
+#                        print(payload)
+                        headers = part.items()
+                        msg_new = set_headers(headers, msg_new)
+                        payload = str(payload)
+
+                        if payload.startswith("b'") and payload.endswith("'"):
+                            payload = payload[2:-1]
+                            bob = msg_new
+                            msg_new.set_payload(payload)
+                            print(bob)
+                            return msg_new
+                        else:
+                            msg_new.set_payload(payload)
+                else:
+                    payload = str(part.get_payload(decode=True))
+                    if payload.startswith("b'") and payload.endswith("'"):
+                        payload =  payload[2:-1]
+                        msg_new.set_payload(payload)
+                ####
         else:
-            payload = str(msg.get_payload(decode=True))
-            if payload.startswith("b'") and payload.endswith("'"):
-                payload =  payload[2:-1]
-        msg_new.set_payload(payload)
+            content_type = msg.get_content_type()
+            charset = msg.get_content_charset()
+
+            if not charset or content_type == 'text/plain':
+                #usually attachments
+                payload = msg.get_payload()
+            else:
+                payload = str(msg.get_payload(decode=True))
+                if payload.startswith("b'") and payload.endswith("'"):
+                    payload =  payload[2:-1]
+            msg_new.set_payload(payload)
     return msg_new
 
 for filename in mbox_files:
