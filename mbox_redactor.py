400ce9564d5918bc969a7787296d5dcc8cc59f5f
Andrew McLaughlin <andrew@mclau.ghlin.com>
2018-01-13 00:00:30 -0800

    Fixed recursive function calls. Also headers now print correctly.

--- mbox_redactor.py
+++ mbox_redactor.py
@@ -22,60 +22,60 @@ if os.path.exists(mbox_path_new):
 os.makedirs(mbox_path_new)
 
 def set_headers(headers, msg_new):
-#    print(headers)
-#    input('press key')
     for key, value in headers: 
         msg_new[key] = value
-#        print(f'{key}: {value}')
-#        input('press key')
     return msg_new
 
-def process_payload(headers, payload, msg_new):
-    msg_new = set_headers(headers, msg_new)
-    msg_new.set_payload(payload)
-    return msg_new
-
+#you might have to break each loop into a seperate function
 def multipart_message(msg, msg_new):
-    if isinstance(msg, mailbox.mboxMessage):
-        for part in msg.get_payload():
-            msg_new = process_message(part, msg_new)
-    elif isinstance(msg, email.message.Message):
-        for part in msg.get_payload():
-            print(part)
-#            if isinstance(part, email.message.Message):
+    for part in msg.get_payload():
+        headers = msg.items()
+        set_headers(headers, msg_new)
+        if isinstance(part, email.message.Message):
+            if part.is_multipart():           
+                msg_new = multipart_message(part, msg_new)
+            else:
+                #todo!
+                #rather than assign the output of single_message()
+                #to msg_new, add it to a dictionary. Then loop
+                #through that dictionary at the end under main()
+                #and add it to mbox_new
 
-                #usually attachments
-#                payload = part.get_payload(decode=True) 
-
-#                if isinstance(payload, list):
-#                    for subpart in payload:
-#                        msg_new = single_message(subpart, msg_new)
-#                else:
-#                    #kathy
-#                    msg_new = single_message(part, msg_new)
-#            else:
-#                msg_new = single_message(part, msg_new)
+                #payload is a string()
+                #print(type(part.get_payload()))
+                msg_new = single_message(part, msg_new)
+                print(100*'#')
+                print(msg_new)
+                #input('click')
+        else:
+            msg_new = single_message(part, msg_new)
+    #print(75*'#')
+    #print(msg_new)
     return msg_new
 
 def single_message(msg, msg_new):
+ 
     content_type = msg.get_content_type()
-    charset = msg.get_content_charset()
-    headers = msg.items()
-    msg_new = set_headers(headers, msg_new)
-
+    charset = msg.get_content_charset() 
+    headers = msg.items() 
+    set_headers(headers, msg_new)
+ 
     if not charset or content_type == 'text/plain':
         #usually attachments
         payload = msg.get_payload()
     else:
         payload = str(msg.get_payload(decode=True))
         if payload.startswith("b'") and payload.endswith("'"):
-            payload =  payload[2:-1]
-    msg_new.set_payload(payload)
+            payload = payload[2:-1]
+
+    #print(type(payload))
+    if payload:
+        msg_new.set_payload(payload)
     return msg_new
 
  
 def process_message(msg, msg_new):
-
+    #if msg and msg_new:
     if msg.is_multipart():
         msg_new = multipart_message(msg, msg_new)
     else:
@@ -93,17 +93,19 @@ for filename in mbox_files:
 
         for key, value in mbox.iteritems():
             try:
-                #get msg input
                 msg      = mbox[key]
-                unixfrom = msg.get_unixfrom()
-                #headers  = msg.items()
-
-                #output
                 msg_new  = mailbox.mboxMessage()
-                msg_new.set_unixfrom(unixfrom)
+
+                unixfrom = msg.get_unixfrom()
+                if unixfrom:
+                    msg_new = msg_new.set_unixfrom(unixfrom)
+
                 msg_new = process_message(msg, msg_new)
 
-                #write file and finish up
+                #todo!
+                #check to see if mbox_new is a dictionary, if so, loop
+                #through the array and add each element to mbox_new
+
                 mbox_new.add(msg_new)
 
             except (AttributeError, KeyError, UnicodeEncodeError) as e:
